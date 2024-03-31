3b34f39d3230e06174c9301610cc09bc1f49ed8d
kkh3868 <rjsgml198@gmail.com>
2024-03-31 12:20:09 +0900

    1st commit

---
+++ mycarstock_react/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <div className="blue-header">
+        <h1>StockDrive</h1>
+      </div>
+      {/* Login Interface */}
+      <div className="login-container">
+        <h2>Login</h2>
+        <div className="input-container">
+          <input type="text" placeholder="ID" className="login-input" />
+          <input type="password" placeholder="Password" className="login-input" />
+        </div>
+        <div className="button-container">
+          <button className="login-button">Login</button>
+        </div>
+        <div className="link-container">
+          <span className="create-id">Create ID</span>
+          <span className="find-id">Find ID</span>
+          <span className="forgot-password">Forgot Password?</span>
+        </div>
+      </div>
+      {/* 홈페이지 구성의 끝 */}
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
+const app = express()
+const path = require('path')
+
+
+app.listen(8080, function(){
+  console.log('listening on 8080');
+});
+
+app.use(express.static(path.join(__dirname, 'mycarstock_react/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'mycarstock_react/build/index.html'));
+});
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, 'mycarstock_react/build/index.html'));
+});
